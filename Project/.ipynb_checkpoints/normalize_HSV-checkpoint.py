8efda8b35ff3b213215457938b76e618c523dcad
Henning Blomfeldt Thorsen <henky95@gmail.com>
2022-11-23 15:59:28 +0100

    Added brightness-based segmentation masks, more sample images

---
+++ Project/.ipynb_checkpoints/normalize_HSV-checkpoint.py
@@ -0,0 +1,89 @@
+import numpy as np
+import math
+from PIL import Image, ImageChops
+
+
+def normalize_brightness(image, N, object_mask):
+    '''
+    Takes in a PIL image object, and returns a dictionary of N masks that correspond to areas within certain brightness/Value (HSV)-brackets
+    '''
+    o_mask = np.array(object_mask)
+    hsv = RGB_to_HSV(image)
+    brightness_brackets = list(np.linspace(0,1,N))
+    print(brightness_brackets)
+    masks = {}
+    for bracket in brightness_brackets:
+            masks[str(brightness_brackets.index(bracket))] = np.zeros(hsv.shape[0:-1])
+    
+    for y in range(hsv.shape[0]):
+        for x in range(hsv.shape[1]):
+            H, S, V = hsv[y, x, :]
+            applied = False
+            for bracket in brightness_brackets:
+                if V <= bracket and not applied:
+                    applied = True
+                    masks[str(brightness_brackets.index(bracket))][y,x] = min(255, o_mask[y,x])
+    for mask in masks.keys():
+        masks[mask] = Image.fromarray((masks[mask]).astype(np.uint8))
+    return masks
+                    
+    
+    
+def RGB_to_HSV(RGB):
+    '''
+    Performs RGB to HSV conversion on the input Image object RGB and
+    returns HSV representation as a numpy array.
+    '''
+    RGB = np.array(RGB).astype(float) / 255.0
+    HSV = np.zeros(RGB.shape)
+    for y in range(RGB.shape[0]):
+        for x in range(RGB.shape[1]):
+            R, G, B = RGB[y,x,:]
+            V = np.max((R, G, B))
+            m = np.min((R, G, B))
+            C = V - m
+            if V == 0.0:
+                S = 0.0
+            else:
+                S = C / V
+                
+            if C == 0.0:
+                H = 0.0
+            else:
+                if V == R:
+                    H_prime = (G-B) / C
+                elif V == G:
+                    H_prime = (B-R) / C + 2
+                elif V == B:
+                    H_prime = (R-G) / V + 4
+                
+                if H_prime < 0:
+                    H = H_prime / 6 + 1
+                else:
+                    H = H_prime / 6
+            
+            HSV[y,x,0] = H
+            HSV[y,x,1] = S
+            HSV[y,x,2] = V
+    
+    return HSV
+
+
+def HSV_to_RGB(HSV):
+    # Student name: Henning Blomfeldt Thorsen
+    import math
+    rgb = np.zeros(HSV.shape)  # array to hold the rgb-values
+    for y_coord in range(HSV.shape[0]):  # loop through x coordinates
+        for x_coord in range(HSV.shape[1]):  # Loop through Y-coordinates
+            H, S, V = HSV[y_coord, x_coord, :]  # extract HSV - values
+            # Following the algorithm put forth in the paper "Color Gamut Transform Pairs" by Alvy Ray Smith:
+            H *= 6 # H := 6H
+            I = math.floor(H)  # Let I := floor(H)
+            F = H-I  # F := H-I
+            M = V*(1-S)  # M := V*(1-S)
+            N = V*(1-(S*F))  # N := V*(1-S*F)
+            K = V*(1-S*(1-F))  # K := V*(1-S*(1-F))
+            r,g,b = [(V,K,M),(N,V,M),(M,V,K),(M,N,V),(K,M,V),(V,M,N)][I]  # switch case of RGB based on value of I
+            # values are decimals between 0 and 1, multiply by 255 to get valid RGB-values, and insert into image
+            rgb[y_coord, x_coord, :] = [r*255, g*255, b*255]
+    return Image.fromarray((rgb).astype(np.uint8))  # return image with type uint8 for display purposes
--- Project/.ipynb_checkpoints/segmentation-checkpoint.py
+++ Project/.ipynb_checkpoints/segmentation-checkpoint.py
@@ -73,6 +73,7 @@ def blur_object_pixels(model, input_image, objects_to_blur, sigma=1.0, show_obje
     input_image_blurred = transforms.GaussianBlur(kernel_size=(3,3), sigma=sigma)(input_image)
     
     mask = transforms.ToPILImage()(mask).convert('L')
+    display(mask)
     '''
     At this point, mask refers to the detected object
     - We apply our transforms/effects to the WHOLE image, then composite the altered image with the original, using the mask as seen below.
@@ -92,3 +93,76 @@ def blur_object_pixels(model, input_image, objects_to_blur, sigma=1.0, show_obje
         output_image = output_image.resize((width, height))
             
     return output_image
+
+
+def get_mask(model, input_image, objects_to_blur, sigma=1.0, show_object_list=False, concat=False, scale=1):
+    """
+    This function blurs the pixels of the objects listed in 'objects_to_blur' on the input 'input_image'
+    using the semantic segmentation 'model' and returns the modified image. The amount of blur is
+    controlled by the parameter 'sigma'.
+    """
+        
+    model.eval()
+    
+    preprocess = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
+    
+    input_tensor = preprocess(input_image)
+    input_batch = input_tensor.unsqueeze(0)
+    
+    if torch.cuda.is_available():
+        input_batch = input_batch.to('cuda')
+        model.to('cuda')
+    
+    with torch.no_grad():
+        output = model(input_batch)['out'][0]
+    output_predictions = output.argmax(0)
+    
+    objects = {
+        0:'background',
+        1:'aeroplane',
+        2:'bicycle',
+        3:'bird',
+        4:'boat',
+        5:'bottle',
+        6:'bus',
+        7:'car',
+        8:'cat',
+        9:'chair',
+        10:'cow',
+        11:'dining table',
+        12:'dog',
+        13:'horse',
+        14:'motorbike',
+        15:'person',
+        16:'potted plant',
+        17:'sheep',
+        18:'sofa',
+        19:'train',
+        20:'tv/monitor'
+    }
+    
+    objects_segmented = {}
+    
+    for output_prediction in output_predictions.unique().tolist():
+        objects_segmented[output_prediction] = objects[output_prediction]
+    
+    if show_object_list:
+        print(f'objects: {list(objects_segmented.values())}')
+        
+    
+    mask = torch.zeros_like(output_predictions, dtype=torch.uint8)
+    
+    for key in objects_segmented.keys():
+        if objects_segmented[key] in objects_to_blur:
+            index = (output_predictions == key)
+            mask[index] = 255
+    
+    input_image_blurred = transforms.GaussianBlur(kernel_size=(3,3), sigma=sigma)(input_image)
+    
+    mask = transforms.ToPILImage()(mask).convert('L')
+    return mask
